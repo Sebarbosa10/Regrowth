fdeed06ce33abe9dd0a6b133ebfa56a4d8f70cf0
SatoruuGoyoo <166155570+SatoruuGoyoo@users.noreply.github.com>
2026-07-10 14:11:54 -0300

    Update CustomUpdateManager.cs

--- Assets/Scripts/CustomUpdateManager.cs
+++ Assets/Scripts/CustomUpdateManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,11 @@ public class CustomUpdateManager : MonoBehaviour
     public static CustomUpdateManager Instance { get; private set; }
 
     private readonly List<IUpdatable> updatables = new List<IUpdatable>();
-    private readonly Dictionary<IUpdatable, int> updatablesSet = new Dictionary<IUpdatable, int>();
+    private readonly Dictionary<IUpdatable, int> indexMap = new Dictionary<IUpdatable, int>();
 
     private readonly List<IUpdatable> pendingAdds = new List<IUpdatable>();
-    private readonly
+    private readonly List<IUpdatable> pendingRemoves = new List<IUpdatable>();
+    private bool isTicking;
 
     private void Awake()
     {
@@ -17,22 +19,62 @@ public class CustomUpdateManager : MonoBehaviour
         Instance = this;
     }
 
+    private void OnDestroy()
+    {
+        if (Instance == this) Instance = null;
+    }
+
     private void Update()
     {
         float dt = Time.deltaTime;
-        for (int i = updatables.Count - 1; i >= 0; i--)
-            updatables[i].Tick(dt);
+
+        isTicking = true;
+        for (int i = 0; i < updatables.Count; i++)
+        {
+            var u = updatables[i];
+            try
+            {
+                u.Tick(dt);
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e, this);
+            }
+        }
+        isTicking = false;
+
+        FlushPending();
     }
 
     public void Register(IUpdatable updatable)
     {
-        if (updatablesSet.Add(updatable))
-            updatables.Add(updatable);
+        if (updatable == null || indexMap.ContainsKey(updatable)) return;
+
+        if (isTicking)
+        {
+            pendingRemoves.Add(updatable);
+            return;
+        }
+
+        AddImmediate.Add(updatable);
     }
 
     public void Unregister(IUpdatable updatable)
     {
-        if (updatablesSet.Remove(updatable))
-            updatables.Remove(updatable);
+        if (updatable == null || indexMap.ContainsKey(updatable)) return;
+
+        if (isTicking)
+        {
+            pendingRemoves.Add(updatable);
+            return;
+        }
+
+        RemoveImmediate.Add(updatable);
+    }
+
+    private void AddImmediate(IUpdatable updatable)
+    {
+        indexMap[updatable] = updatables.Count;
+        updatables.Add(updatable);
     }
 }
